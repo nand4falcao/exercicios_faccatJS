d8aa10878c36404ef3a25a69fe5e8798a7d9dec5
MARIA FERNANDA FALCAO DE LIMA <maria.fflima14@senacsp.edu.br>
2025-10-02 12:43:51 -0300

    exercicio 7

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let idade = parseInt(prompt("Digite a sua idade: "))
+let meses = parseInt(prompt("Digite os meses: "))
+let dias = parseInt(prompt("Digite os dias: "))
+let total = (idade * 365) + (meses * 30) + dias
+
+alert ("Essa é a sua idade em dias: " + total)
